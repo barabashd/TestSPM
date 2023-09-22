a9e330447e276dfe45eb2ea4075ba2b2d543179e
Dmytro <barabashdmyto@gmail.com>
2023-09-22 06:38:09 -0700

    init all files

---
+++ TestSPM/TestSPMApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSPMApp.swift
+//  TestSPM
+//
+//  Created by Dmytro Barabash1 on 22.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestSPMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
